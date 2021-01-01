13a6ea3ac390a74630ebd3d0cd1c214de4530905
joseph <josephjohndemarco@gmail.com>
2021-01-01 08:55:33 -0500

    Add notes.

---
+++ main.js
@@ -0,0 +1,11 @@
+/* ----- constants ----- */
+
+/* ----- globals ----- */
+
+/* ----- state variables ----- */
+
+/* ----- cached element references ----- */
+
+/* ----- event listeners ----- */
+
+/* ----- functions ----- */
