79ffe10ce92608fce367bc74f4915304024199b2
joseph <josephjohndemarco@gmail.com>
2021-01-02 08:28:34 -0500

    Add keypress event.

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-/* ----- constants ----- */
+document.addEventListener('DOMContentLoaded', () => {
+  /* ----- state variables ----- */
+  let shipLeft = 200;
+  let shipBottom = 100;
+  let gravity = 2;
+  let isGameOver = false;
 
-/* ----- globals ----- */
+  /* ----- cached element references ----- */
+  const ship = document.querySelector('.ship');
 
-/* ----- state variables ----- */
+  /* ----- event listeners ----- */
+  document.addEventListener('keydown', control);
 
-/* ----- cached element references ----- */
+  /* ----- functions ----- */
+  // function start() {
+  //   ship.style.bottom = shipBottom + 'px';
+  //   ship.style.left = shipLeft + 'px';
+  // }
 
-/* ----- event listeners ----- */
+  function control(e) {
+    if (e.keyCode === 37) {
+      console.log(e.target);
+      // go left
+      // move('L');
+    } else if (e.keyCode === 38) {
+      // go up
+      // move('U');
+    } else if (e.keyCode === 39) {
+      // go right
+      // move('R');
+    } else if (e.keyCode === 40) {
+      // go down
+      move('D');
+    }
+  }
 
-/* ----- functions ----- */
+  function move(direction) {
+    if (direction === 'D') {
+      shipBottom += 10;
+      ship.style.bottom = shipBottom - 'px';
+      console.log(shipBottom);
+    }
+  }
+
+})
