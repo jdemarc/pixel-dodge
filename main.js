1960f8e65862b42e071b7f1017a119e479d4f284
joseph <josephjohndemarco@gmail.com>
2021-01-03 08:27:55 -0500

    Add boundaries to ship movement.

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   /* ----- state variables ----- */
-  let shipLeft = 225;
+  let shipLeft = 220;
   let shipBottom = 100;
-  let gravity = 2;
   let isGameOver = false;
 
   /* ----- cached element references ----- */
@@ -17,22 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
   //   ship.style.left = shipLeft + 'px';
   // }
 
-  // function control(e) {
-  //   if (e.keyCode === 37) {
-  //     // go left
-  //     // move('L');
-  //   } else if (e.keyCode === 38) {
-  //     // go up
-  //     // move('U');
-  //   } else if (e.keyCode === 39) {
-  //     // go right
-  //     // move('R');
-  //   } else if (e.keyCode === 40) {
-  //     // go down
-  //     move('D');
-  //   }
-  // }
-
   function control(e) {
     (e.keyCode === 37) ? move('L')
     : (e.keyCode === 38) ? move ('U')
@@ -43,22 +26,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function move(direction) {
     if (direction === 'L') {
-      shipLeft -= 10;
+      if (shipLeft > 0) shipLeft -= 10;
     } else if (direction === 'U') {
-      shipBottom += 10;
+      if (shipBottom < 550) shipBottom += 10;
     } else if (direction === 'R') {
-      shipLeft += 10;
+      if (shipLeft < 450) shipLeft += 10;
     } else if (direction === 'D') {
-      shipBottom -= 10;
+      if (shipBottom > 0) shipBottom -= 10;
     }
 
     ship.style.left = shipLeft + 'px';
     ship.style.bottom = shipBottom + 'px';
-
-    // if (direction === 'D') {
-    //   shipBottom -= 10;
-    //   ship.style.bottom = shipBottom + 'px';
-    // }
 
     console.log('S.left: ', shipLeft);
     console.log('S.bottom: ', shipBottom);
